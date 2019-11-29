dee3be4dcedd39bbae54ff222d87c09f1fdd38d7
Aaron Kovacs <aaronk4576@gmail.com>
2019-11-29 01:54:21 -0500

    Update network-monitor.sh

--- network-monitor.sh
+++ network-monitor.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 while true ; do
-    kill $(pgrep -f 'python error.py')
     if ifconfig wlan0 | grep -q "inet addr:" ; then
         sleep 60
     else
         echo "Network connection down! Attempting reconnection."
-        python /home/pi/2048-Pi-Display/error.py &
         ifup --force wlan0
         sleep 10
     fi
