45d569650147c08cbf4e9f48d95b5188ba04af7d
Aaron Kovacs <aaronk4576@gmail.com>
2019-12-07 21:50:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -176,7 +176,8 @@ def fetchSpotify():
     t.start()
 
     with open('/home/pi/2048-Pi-Display/spotify.txt', 'r') as json_file:
-        result = json.load(json_file.read())
+        print(json_file)
+        result = json.load(json_file)
         if result is not None and "is_playing" in result:
             is_playing = result["is_playing"]
             if currentTrack != result["item"]["name"] and result["item"]["name"] != '':
