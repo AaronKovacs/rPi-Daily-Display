775dd5e53686695c3ff3ce87d1f71526b534c593
Aaron Kovacs <aaronk4576@gmail.com>
2020-02-06 17:57:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -118,8 +118,14 @@ class rPiDisplay(DisplayBase):
                     rain_color = graphics.Color(92, 200, 250)
                     if iteration % 2 == 0:
                         rain_coords = []
-                        for x in range(0, 10):
-                            rain_coords.append(randomOffset(32, 8))                        
+                        for index in range(0, len(rain_coords)):
+                            if rain_coords[index][1] > 16 + 8:
+                                rain_coords[index] = randomOffset(32, 8)
+                            else:
+                                rain_coords[index][1] += 1
+                        if len(rain_coords) == 0:
+                            for x in range(0, 10):
+                                rain_coords.append(randomOffset(32, 8))                        
                     for coord in rain_coords:
                         offscreen_canvas.SetPixel(coord[0], coord[1] + 16, 92, 200, 250)
                 graphics.DrawText(offscreen_canvas, font, 0, 22, weather_color, currentWeather)
