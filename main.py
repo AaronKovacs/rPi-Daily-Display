b9ee6fa60304f0de7c6b64ac9d71755c02b29032
Aaron Kovacs <aaronk4576@gmail.com>
2020-02-19 16:35:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -311,19 +311,17 @@ def pongPosition(lastCoord, xDir, yDir):
         new_xDir = -new_xDir
 
     # Top Left Corner
-    print(lastCoord)
     if lastCoord == [1,-7]:
-        return pongPosition(randomOffset(31, 6), xDir, yDir)
+        return (randomOffset(31, 6), xDir, yDir)
     # Top Right Corner
     if lastCoord == [31,-7]:
-        return pongPosition(randomOffset(31, 6), xDir, yDir)
+        return (randomOffset(31, 6), xDir, yDir)
     # Bottom Left Corner
     if lastCoord == [1,6]:
-        return pongPosition(randomOffset(31, 6), xDir, yDir)
-
+        return (randomOffset(31, 6), xDir, yDir)
     # Bottom Right Corner
     if lastCoord == [31,6]:
-        return pongPosition(randomOffset(31, 6), xDir, yDir)
+        return (randomOffset(31, 6), xDir, yDir)
 
     if new_xDir != xDir or new_yDir != yDir:
         return pongPosition(lastCoord, new_xDir, new_yDir)
