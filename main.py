3e9237b1c6fd1693b3b447d136e5cbcc57f522ce
Aaron Kovacs <aaronk4576@gmail.com>
2020-09-08 10:49:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -378,7 +378,7 @@ def pongPosition(lastCoord, xDir, yDir, is_playing):
         def inAlbumArt(coord):
             return coord[0] >= 0 and coord[0] <= 8 and coord[1] >= 15 and coord[1] <= 23
 
-        if inAlbumArt(coord):
+        if inAlbumArt(newCoord):
             if newCoord[1] >= 15:
                 new_xDir = -new_xDir
             else:
