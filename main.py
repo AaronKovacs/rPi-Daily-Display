b6e5c2f8f7f1578a83212132f5d54d2dc5955c28
Aaron Kovacs <aaronkovacsdev@gmail.com>
2022-12-07 15:10:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class rPiDisplay(DisplayBase):
                         continue
                     # Insert new icon
                     if row[2] == 0:
-                        col.insert((randomIcon(), 8 * column, -8), 0)
+                        col.insert(0, (randomIcon(), 8 * column, -8))
 
                     # Move
                     row[2] += 1 *  vels[col]
