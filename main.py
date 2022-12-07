c55010febc8dbf2e386d1fcf51591375dc5fca40
Aaron Kovacs <aaronkovacsdev@gmail.com>
2022-12-07 15:20:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class rPiDisplay(DisplayBase):
         cols = [[], [], []]
 
         # slowest to fastest: 60, 
-        vels = [1.2, 1.2, 1.2]
+        vels = [1, 1, 1]
 
         for column in range(3):
             for i in range(10):
