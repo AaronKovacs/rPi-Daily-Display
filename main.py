975d708d09a319cd60179e7e12cab3455e80a373
Aaron Kovacs <aaronkovacsdev@gmail.com>
2022-12-07 21:35:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ class rPiDisplay(DisplayBase):
                         break
 
             if state == 1:
-                vels = [3, 5, 4]
+                vels = [6, 5, 6]
                 state = 2
 
             # After 5s start slowdown
